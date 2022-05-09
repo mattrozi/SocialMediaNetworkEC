39dbd4df169a84abfe849ab21b46cc53ed44f628
Matt Rozinskas <matt_rozinskas1@baylor.edu>
2022-05-09 17:26:05 -0500

    Changes made after video 1

--- FeedApp/urls.py
+++ FeedApp/urls.py
@@ -5,6 +5,8 @@ app_name = 'FeedApp'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('profile/',views.profile,name='profile'),
+    path('myfeed',views.myfeed,name='myfeed'),
     
     ]
 
